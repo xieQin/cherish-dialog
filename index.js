08a6294d81e3f67e3025aafa8e979a4d43a50712
cherish <cherishhsieh@gmail.com>
2016-06-19 12:18:24 +0800

    add cherish-dialog

---
+++ index.js
@@ -0,0 +1,6 @@
+import './src/css/normalize.css'
+import $ from 'jquery'
+import './src/css/main.css'
+import './node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+$('#app').html('<p>111</p>')
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?noInfo=true&reload=true',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel!eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+        include: __dirname
+      }
+    ]
+  }
+}
